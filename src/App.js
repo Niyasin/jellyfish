a3ad8bb3ffe081888c9f0da0cbb423774d47dea5
Niyasin <mo.niyas2000@gmail.com>
2023-02-05 10:34:18 +0530

    form 📃

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 function App() {
   return (
-    <div className="App">
-    </div>
+    <div className="container">
+      <div className="form">
+        <h1>Registration</h1>
+        <div className="inputwrap">
+          <div className="horizontal">
+            <input placeholder="First Name"/>
+            <input placeholder="Last Name"/>
+          </div>
+          <input placeholder="Email"/>
+          <input placeholder="Phone"/>
+          <div className="horizontal">
+            <input placeholder="Pronouns"/>
+            <input placeholder="Date Of Boirth"/>
+          </div>
+          <input placeholder="Address"/>
+        <div className="button">Submit</div>
+        </div>
+      </div>
+      <div class="imagewrap">
+        <img src="./1.png"/>
+      </div>
+      </div>
   );
 }
 
